42c47e1fd5b5947b4ff8acf298f20dcfbea3cee1
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-15 19:04:04 -0500

    output device Class definition

--- main.js
+++ main.js
@@ -18,3 +18,23 @@ class InputDevice{
         this._brand=brand
     }
 }
+class OutputDevice{
+    static outputDevicesCounter=0
+    constructor({outputType,brand}){
+        ++OutputDevice.outputDevicesCounter;
+        this._outputType=outputType;
+        this._brand=brand;
+    }
+    get outputType(){
+        return this._outputType
+    }
+    set outputType(outputType){
+        this._outputType=outputType
+    }
+    get brand(){
+        return this._brand
+    }
+    set brand(brand){
+        this._brand=brand
+    }
+}
